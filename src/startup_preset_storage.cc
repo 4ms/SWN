69874f91e35b4b629c4bda84ff60f6041977e81c
Dan Green <danngreen1@gmail.com>
2020-08-10 14:35:34 -0700

    Fixed collision on flash if WT and startup preset save access at same time

--- src/startup_preset_storage.cc
+++ src/startup_preset_storage.cc
@@ -3,6 +3,7 @@
 #include <stdint.h>
 extern "C" {
 #include "external_flash_layout.h"
+#include "timekeeper.h"
 }
 
 extern "C" uint16_t get_startup_preset();
@@ -31,13 +32,26 @@ static Persistent<WearLevel<FlashStorage<STARTUP_PRESET_SETTING_SECTOR, StartupP
 
 extern "C" void init_startup_preset_storage(void)
 {
+	pause_timer_IRQ(OSC_TIM_number);
+	pause_timer_IRQ(WT_INTERP_TIM_number);
+	pause_timer_IRQ(PWM_OUTS_TIM_number);
 	startup_preset_storage.init(default_startup_preset);
+	resume_timer_IRQ(OSC_TIM_number);
+	resume_timer_IRQ(WT_INTERP_TIM_number);
+	resume_timer_IRQ(PWM_OUTS_TIM_number);
 }
 extern "C" void set_startup_preset(uint16_t preset_num)
 {
 	startup_preset.preset_num = preset_num;
 	startup_preset.check_word = CHECK_WORD;
+
+	pause_timer_IRQ(OSC_TIM_number);
+	pause_timer_IRQ(WT_INTERP_TIM_number);
+	pause_timer_IRQ(PWM_OUTS_TIM_number);
 	startup_preset_storage.Save();
+	resume_timer_IRQ(OSC_TIM_number);
+	resume_timer_IRQ(WT_INTERP_TIM_number);
+	resume_timer_IRQ(PWM_OUTS_TIM_number);
 }
 extern "C" uint16_t get_startup_preset()
 {
